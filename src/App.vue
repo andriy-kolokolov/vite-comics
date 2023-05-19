6d1c72a3b58f3f3a9071e1ddb9ef6f0d7c1e9255
Andriy-Kolokolov <koloandre22@gmail.com>
2023-05-19 11:49:39 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="test">HELLO WORLD</div>
+</template>
+
+<style lang="sass">
+.test
+  background-color: #00df64
+  padding: 10px
+  text-align: center
+</style>
